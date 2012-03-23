09c983eee69a0742e9b41224b34b1a1ff2537ade
Adrien Lemaire <lemaire.adrien@gmail.com>
2012-03-23 11:43:52 +0900

    Test wasn't relevant enough, this fix makes it much faster as well.
    Please notice that the Pypi version of django-longerusername is broken. Consider updating it, thanks

--- longerusername/tests.py
+++ longerusername/tests.py
@@ -5,15 +5,10 @@ class LongerUsernameTests(TestCase):
     """
     Unit tests for longerusername app
     """
-    def setUp(self):
-        """
-        creates a user with a terribly long username
-        """
-        long_username = ''.join([str(i) for i  in range(100)])
-        self.user = User.objects.create_user('test' + long_username, 'test@test.com', 'testpassword')
+
     def testUserCreation(self):
         """
-        tests that self.user was successfully saved, and can be retrieved
+        tests that User.username field has now a max_length = 255 instead of the django default 30
         """
-        self.assertNotEqual(self.user,None)
-        User.objects.get(id=self.user.id) # returns DoesNotExist error if the user wasn't created
+        self.assertEqual(User._meta.get_field('username').max_length, 255,
+        msg='user.signals changed the max_length from 30 to 255')
